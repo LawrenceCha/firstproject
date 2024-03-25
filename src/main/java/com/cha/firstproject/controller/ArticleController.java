e0f15118483f08e69d11b76234cf462ea40c6aa0
LawrenceCha <lawrencecha93@gmail.com>
2024-03-25 11:56:13 +0900

    add Article List

--- src/main/java/com/cha/firstproject/controller/ArticleController.java
+++ src/main/java/com/cha/firstproject/controller/ArticleController.java
@@ -3,12 +3,19 @@ package com.cha.firstproject.controller;
 import com.cha.firstproject.dto.ArticleForm;
 import com.cha.firstproject.entity.Article;
 import com.cha.firstproject.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+import java.util.Optional;
+
 @Controller
+@Slf4j // simple login 로깅을 위한 어노테이션
 public class ArticleController {
 
     @Autowired
@@ -20,15 +27,45 @@ public class ArticleController {
 
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form) {
-        System.out.println(form.toString());
+        log.info(form.toString());
+        // System.out.println(form.toString()); -> 로깅기능으로 대체
+
 
         // 1. Dto를 Entity 변환
         Article article = form.toEntity();
-        System.out.println(article.toString());
+        log.info(article.toString());
+        //System.out.println(article.toString());
 
         // 2. Repository에게 Entity를 DB로 저장하게 함
         Article saved = articleRepository.save(article);
-        System.out.println(saved.toString());
-        return "";
+        log.info(saved.toString());
+        //System.out.println(saved.toString());
+        return "redirect:/articles/" + saved.getId();
     }
+
+    @GetMapping("/articles/{id}") // 해당 URL요청을 처리 선언
+    public String show(@PathVariable Long id, Model model) { // URL에서 id를 변수로 가져옴
+        log.info("id = " + id);
+
+        // 1: id로 데이터를 가져옴
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+        //Optional<Article> articleEntity = articleRepository.findById(id);와 동일
+
+        // 2: 가져온 데이터를 모델에 등록
+        model.addAttribute("article", articleEntity);
+
+        // 3: 보여줄 페이지를 설정
+        return "articles/show";
+    }
+
+    @GetMapping("/articles")
+    public String index(Model model) {
+        // 1: 모든 Article을 가져온다!
+        List<Article> articleEntityList = articleRepository.findAll();
+        // 2: 가져온 Article 묶음을 뷰로 전달!
+        model.addAttribute("articleList", articleEntityList);
+        // 3: 뷰 페이지를 설정!
+        return "articles/index";
+    }
+
 }
